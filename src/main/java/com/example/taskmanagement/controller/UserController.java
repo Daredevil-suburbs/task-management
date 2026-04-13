d877e9f3125d050977df07d4f43da4e2df0f5b1f
Daredevil-suburbs <armaansheikh3108@gmail.com>
2026-04-13 22:18:41 +0530

    chore: sync src/main/java/com/example/taskmanagement/controller/UserController.java

--- src/main/java/com/example/taskmanagement/controller/UserController.java
+++ src/main/java/com/example/taskmanagement/controller/UserController.java
@@ -4,7 +4,6 @@ import com.example.taskmanagement.dto.UserDTO;
 import com.example.taskmanagement.dto.UserStatusDTO;
 import com.example.taskmanagement.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
