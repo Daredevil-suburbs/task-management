2cea6054523e9e1b301c843084dc5e0bc66b98dd
Daredevil-suburbs <armaansheikh3108@gmail.com>
2026-04-13 22:20:32 +0530

    chore: sync src/main/java/com/example/taskmanagement/service/UserService.java

--- src/main/java/com/example/taskmanagement/service/UserService.java
+++ src/main/java/com/example/taskmanagement/service/UserService.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.List;
 import java.util.UUID;
 
 @Service
