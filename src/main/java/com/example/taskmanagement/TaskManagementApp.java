35480cb588c37b85a153f5f6f5e2e22ecc2ac9e2
Daredevil-suburbs <armaansheikh3108@gmail.com>
2026-04-13 22:12:23 +0530

    chore: sync src/main/java/com/example/taskmanagement/TaskManagementApp.java

---
+++ src/main/java/com/example/taskmanagement/TaskManagementApp.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class TaskManagementApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementApp.class, args);
+    }
+}
