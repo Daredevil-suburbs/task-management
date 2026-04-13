e1d75079d6c7aa482802eed2a2bfb216250b9ecd
Daredevil-suburbs <armaansheikh3108@gmail.com>
2026-04-13 22:21:29 +0530

    chore: sync src/test/java/com/example/taskmanagement/controller/TaskControllerIntegrationTest.java

--- src/test/java/com/example/taskmanagement/controller/TaskControllerIntegrationTest.java
+++ src/test/java/com/example/taskmanagement/controller/TaskControllerIntegrationTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles("test")
 class TaskControllerIntegrationTest {
 
     @Autowired private MockMvc mockMvc;
