d71430b20f5b37727af33ab724b429433a24e244
Daredevil-suburbs <armaansheikh3108@gmail.com>
2026-04-14 23:45:59 +0530

    chore: sync frontend/src/components/index.js

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,10 +1,9 @@
-/** 
- * @author Daredevil-suburbs
- */
-// The Hunter System - Component Exports
-// Gamified Task Management Components inspired by Solo Leveling
-
+// Hunter System Components
 export { default as QuestDashboard } from './QuestDashboard';
 export { default as AddQuestModal } from './AddQuestModal';
 export { default as PlayerStatsPanel } from './PlayerStatsPanel';
 export { default as HealthSyncDashboard } from './HealthSyncDashboard';
+export { default as StatusPanel } from './StatusPanel';
+export { default as AddQuest } from './AddQuest';
+export { default as QuestList } from './QuestList';
+export { default as QuestCard } from './QuestCard';
